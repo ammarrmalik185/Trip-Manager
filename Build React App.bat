8af838e05717ba22bb9449e426e5a04bba6efbf2
Ammar Malik <ammarrmalik185@hotmail.com>
2024-06-13 18:33:38 +0500

    feat: update build configs

--- Build React App.bat
+++ Build React App.bat
@@ -1,5 +1,1 @@
 react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
-cd android
-./gradlew assembleDebug
-
-echo "APK is in react_app/android/app/build/outputs/apk/debug/app-debug.apk";
