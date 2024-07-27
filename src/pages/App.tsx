4ff2a74a0964a81867cbb29f4c164c2da69d42f6
Ammar Malik <ammarrmalik185@hotmail.com>
2024-07-27 20:53:59 +0500

    feat: add env(sample) file and helpers

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import 'dotenv/config';
+
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
