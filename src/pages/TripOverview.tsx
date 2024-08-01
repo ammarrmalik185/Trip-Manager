6b2800bf5b216a9e1ff069bface0c33b9a43baed
Ammar Malik <ammarrmalik185@hotmail.com>
2024-08-01 18:54:06 +0500

    feat: add sections for trip overview

--- src/pages/TripOverview.tsx
+++ src/pages/TripOverview.tsx
@@ -6,6 +6,7 @@ import Toast from "react-native-simple-toast";
 function TripOverview({route, navigation}:any) {
     return (
         <View style={styles.main}>
+            <View style={styles.item}>
             <View style={styles.detailsDisplay}>
                 <Text style={styles.title}>{route.params.trip.title}</Text>
                 <Text style={styles.subTitle}>{route.params.trip.destination}</Text>
@@ -14,71 +15,60 @@ function TripOverview({route, navigation}:any) {
                 <Text style={styles.subTitle}>Member Count: {route.params.trip.members.length}</Text>
                 <Text style={styles.subTitle}>Total Expenses: Rs. {route.params.trip.expenses.reduce((acc:any, expense:any) => acc + expense.amount, 0)}</Text>
             </View>
+            </View>
             <View style={styles.buttonsContainer}>
                 <TouchableOpacity style={styles.acceptButton} onPress={() => navigation.navigate(pages.TripDetails, {trip: route.params.trip})}>
                     <Text style={styles.acceptButtonText}>Details</Text>
                 </TouchableOpacity>
-                <View style={styles.horizontalStack}>
-                    <TouchableOpacity style={styles.acceptButtonMax} onPress={() => navigation.navigate(pages.TripMembers, {trip: route.params.trip})}>
+                <View style={styles.item}>
+                    {/*<Text style={styles.acceptButtonText}>Members</Text>*/}
+                    <View style={styles.horizontalStack}>
                         <Text style={styles.acceptButtonText}>Members</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.addButtonInline} onPress={() => navigation.navigate(pages.TripMembersCreate, {trip: route.params.trip})}>
-                        <Text style={styles.fabText}>+</Text>
-                    </TouchableOpacity>
+                        <TouchableOpacity style={styles.addButtonInline} onPress={() => navigation.navigate(pages.TripMembers, {trip: route.params.trip})}>
+                            <Text style={styles.acceptButtonText}>📃</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.addButtonInline} onPress={() => navigation.navigate(pages.TripMembersCreate, {trip: route.params.trip})}>
+                            <Text style={styles.fabText}>+</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
-                <View style={styles.horizontalStack}>
-                    <TouchableOpacity style={styles.acceptButtonMax} onPress={() => navigation.navigate(pages.TripLogs, {trip: route.params.trip})}>
-                        <Text style={styles.acceptButtonText}>Logs</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.addButtonInline} onPress={() => navigation.navigate(pages.TripLogsCreate, {trip: route.params.trip})}>
-                        <Text style={styles.fabText}>+</Text>
-                    </TouchableOpacity>
+                <View style={styles.item}>
+                    <View style={styles.horizontalStack}>
+                        <Text style={styles.acceptButtonText}>Logs         </Text>
+                        <TouchableOpacity style={styles.addButtonInline} onPress={() => navigation.navigate(pages.TripLogs, {trip: route.params.trip})}>
+                            <Text style={styles.acceptButtonText}>📃</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.addButtonInline} onPress={() => navigation.navigate(pages.TripLogsCreate, {trip: route.params.trip})}>
+                            <Text style={styles.fabText}>+</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
-                <View style={styles.horizontalStack}>
-                    <TouchableOpacity style={styles.acceptButtonMax} onPress={() => navigation.navigate(pages.TripExpenses, {trip: route.params.trip})}>
-                        <Text style={styles.acceptButtonText}>Expenses</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.addButtonInline} onPress={() => {
-                        // navigation.navigate(pages.TripExpensesCreate, {trip: route.params.trip}
-                        if (route.params.trip.members.length > 0){
-                            navigation.navigate(pages.TripExpensesCreate, {trip: route.params.trip})
-                        }else{
-                            Toast.show("Add members before adding expenses", Toast.LONG);
-                        }
-                    }}>
-                        <Text style={styles.fabText}>+</Text>
-                    </TouchableOpacity>
+                <View style={styles.item}>
+                    <View style={styles.horizontalStack}>
+                        <Text style={styles.acceptButtonText}>Expenses </Text>
+                        <TouchableOpacity style={styles.addButtonInline} onPress={() => navigation.navigate(pages.TripExpenses, {trip: route.params.trip})}>
+                            <Text style={styles.acceptButtonText}>📃</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.addButtonInline} onPress={() => {
+                            if (route.params.trip.members.length > 0){
+                                navigation.navigate(pages.TripExpensesCreate, {trip: route.params.trip})
+                            }else{
+                                Toast.show("Add members before adding expenses", Toast.LONG);
+                            }
+                        }}>
+                            <Text style={styles.fabText}>+</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.horizontalStack}>
+                        <TouchableOpacity style={styles.acceptButtonMax} onPress={() => navigation.navigate(pages.TripExpensesComputed, {trip: route.params.trip})}>
+                            <Text style={styles.acceptButtonText}>Computed</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.acceptButtonMax} onPress={() => navigation.navigate(pages.TripExpensesSettle, {trip: route.params.trip})}>
+                            <Text style={styles.acceptButtonText}>Settle</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
-                <TouchableOpacity style={styles.acceptButton} onPress={() => navigation.navigate(pages.TripExpensesComputed, {trip: route.params.trip})}>
-                    <Text style={styles.acceptButtonText}>Computed Expenses</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.acceptButton} onPress={() => navigation.navigate(pages.TripExpensesSettle, {trip: route.params.trip})}>
-                    <Text style={styles.acceptButtonText}>Settle Expenses</Text>
-                </TouchableOpacity>
-                {/*<TouchableOpacity style={styles.acceptButton} onPress={() => {navigation.navigate(pages.TripEdit, {trip: route.params.trip})}}>*/}
-                {/*    <Text style={styles.acceptButtonText}>Edit</Text>*/}
-                {/*</TouchableOpacity>*/}
-                {/*<TouchableOpacity style={styles.declineButton} onPress={() => {*/}
-                {/*    route.params.trip.deleteTrip().then(() => {*/}
-                {/*        trip.allTrips = trip.allTrips.filter(item => item.id != route.params.trip.id);*/}
-                {/*        navigation.navigate(pages.TripList)*/}
-                {/*    });*/}
-                {/*}}>*/}
-                {/*    <Text style={styles.acceptButtonText}>Delete</Text>*/}
-                {/*</TouchableOpacity>*/}
             </View>
-
-            {/*<TouchableOpacity style={styles.fabTop} onPress={() => {navigation.navigate(pages.TripEdit, {trip: route.params.trip})}}>*/}
-            {/*    <Text style={styles.fabText}>✏️</Text>*/}
-            {/*</TouchableOpacity>*/}
-            {/*<TouchableOpacity style={styles.fabNegative} onPress={() => {*/}
-            {/*    route.params.trip.deleteTrip().then(() => {*/}
-            {/*        trip.allTrips = trip.allTrips.filter(item => item.id != route.params.trip.id);*/}
-            {/*        navigation.navigate(pages.TripList)*/}
-            {/*    });*/}
-            {/*}}>*/}
-            {/*    <Text style={styles.fabText}>🗑️</Text>*/}
-            {/*</TouchableOpacity>*/}
         </View>
     )
 }
